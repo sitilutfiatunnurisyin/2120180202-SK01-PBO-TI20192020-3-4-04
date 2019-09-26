44df1738280b0501449ba5a658e6db8af4c75c9f
sitilutfiatunnurisyin <55843840+sitilutfiatunnurisyin@users.noreply.github.com>
2019-09-26 21:52:00 +0700

    Aaploud projek 4

---
+++ src/pkg2120180202/sk01/pbo/ti20192020/pkg3/pkg4/pkg04/SK01PBOTI201920203404.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2120180202.sk01.pbo.ti20192020.pkg3.pkg4.pkg04;
+
+/**
+ *
+ * @author asus
+ */
+public class SK01PBOTI201920203404 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Siti Lutfiatun N
+        for (int i = 1; i <= 10; i++) { 
+            for (int j = i; j <= 9; j++) { 
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= (i * 2) - 2; k++) { 
+                System.out.print("4");
+            }
+            System.out.println();
+        }
+    }
+    
+}
